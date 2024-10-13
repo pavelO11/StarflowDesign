3a6c95dc2cb60c932ce9c95f9ef34c100cd26521
p1baby <p1babywork@gmail.com>
2024-10-13 23:27:24 +0300

    Contact Page code improvements

--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -20,17 +20,17 @@ function Contacts() {
 	useSplittingOnLoad('.slide-vertical');
 	useSplittingHover();
 
-	  useEffect(() => {
+	useEffect(() => {
     Splitting({ target: '.contactButtons' });
     
     const descriptions = document.querySelectorAll('.discription');
     descriptions.forEach(description => {
-      description.outerHTML = `<span class="discription">${description.textContent}</span>`;
+    	description.outerHTML = `<span class="discription">${description.textContent}</span>`;
     });
 
     const initialDelay = 3800; // delay
     setTimeout(() => {
-      setVisibleLines([0, 1]);
+    	setVisibleLines([0, 1]);
     }, initialDelay);
   }, []);
 	
@@ -38,16 +38,16 @@ function Contacts() {
 		<Curve>
 		  <section className='contactSection'>
 			<AnimatePresence mode='wait'>
-			  {brifOpened && <PopupBrif onClose={() => setBrifOpened(false)} selectedService={null} />}
+				{brifOpened && <PopupBrif onClose={() => setBrifOpened(false)} selectedService={null} />}
 			</AnimatePresence>
 			<h1 data-splitting className='slide-vertical'>
-			  <span className='firText'>Давайте</span>
-			  <span className='secText'> Сотрудничать</span>
+				<span className='firText'>Давайте</span>
+				<span className='secText'> Сотрудничать</span>
 			</h1>
 			<h1 className='second'>
-			  <span className='firText'>Давайте</span>
-			  <br />
-			  <span className='secText'>Сотрудничать</span>
+				<span className='firText'>Давайте</span>
+			  	<br />
+				<span className='secText'>Сотрудничать</span>
 			</h1>
 			<NavigationButtons />
 			<article className='contactText'>
@@ -58,24 +58,22 @@ function Contacts() {
 				  rel='noopener noreferrer'
 				  className='contactButtons'
 				>
-				  <p className={visibleLines.includes(0) ? 'visible' : ''} >
-					<span className='char'>СВЯЗАТЬСЯ</span>
-					<span className='char'>СО МНОЙ</span>
-					<span className='discription'>( телеграм )</span>
-				  </p>
-				  {/* <img className='contactLight' alt='light' src='/contactLight.svg' /> */}
+					<p className={visibleLines.includes(0) ? 'visible' : ''} >
+						<span className='char'>СВЯЗАТЬСЯ</span>
+						<span className='char'>СО МНОЙ</span>
+						<span className='discription'>( телеграм )</span>
+				  	</p>
 				</Link>
 				<a
 				  data-splitting
 				  onClick={handleOpenPopup}
 				  className='contactButtons'
 				>
-				  <p className={visibleLines.includes(0) ? 'visible' : ''} >
-					<span className='char'>ЗАПОЛНИТЬ</span>
-					<span className='char'>БРИФ</span>
-					<span className='discription'>( небольшой )</span>
-				  </p>
-				  {/* <img className='contactLight' alt='light' src='/contactLight.svg' /> */}
+					<p className={visibleLines.includes(0) ? 'visible' : ''} >
+						<span className='char'>ЗАПОЛНИТЬ</span>
+						<span className='char'>БРИФ</span>
+						<span className='discription'>( небольшой )</span>
+					</p>
 				</a>
 			</article>
 		  </section>
