3988c65177a03c8592d9d65ad72378ee4d9cd1b5
p1baby <lightuponce@gmail.com>
2025-01-02 20:52:01 +0500

    services minor text fixes

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -17,6 +17,17 @@ import PopupBrif from '../../components/popupbrif/PopupBrif'
 const Services = () => {
   const [brifOpened, setBrifOpened] = useState(false);
   const [selectedService, setSelectedService] = useState<string | null>(null);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+    useEffect(() => {
+    const handleResize = () => {
+        setIsMobileView(window.innerWidth <= 428);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
   const handleOpenPopup = (id: string) => {
     setSelectedService(id);
@@ -47,13 +58,13 @@ const Services = () => {
   }, [myServices]);
 
   const stages = [
-    { id: '1', number: '01', title: 'Знакомство и бриф', description: ['Созвонимся, познакомимся и обсудим детали проекта,', 'чтобы я мог оценить стоимость и сроки.'], time: 'Займет 20-30 минут' },
-    { id: '2', number: '02', title: 'Анализ и структура', description: ['На этом этапе я изучаю целевую аудиторию, анализирую', 'рынок и конкурентов, составляю грамотную структуру,', 'чтобы предложить вам наиболее выигрышный вариант', 'реализации сайта'], time: 'От 2-х дней' },
-    { id: '3', number: '03', title: 'Референсы и прототип', description: ['Я собираю все ваши пожелания по сайту, подбираю', 'качественные референсы, которые мы вместе и', 'утверждаем. После чего переходим к созданию прототипа.'], time: 'От 2-х дней' },
-    { id: '4', number: '04', title: 'Создание дизайн-концепции', description: ['Дизайню 1-3 ключевые страницы, в которых я', 'прорабатываю визуал будущего сайта. Этот этап помогает', 'найти общее виденье и создать грамотную концепцию.'], time: 'От 3-х дней' },
-    { id: '5', number: '05', title: 'Дизайн всего сайта и адаптивы', description: ['Отрисовываю весь сайт целиком, включая внутренние', 'страницы и их состояния в согласованном стиле. Рисую', 'адаптивные версии.'], time: 'От 4-х дней' },
-    { id: '6', number: '06', title: 'Верстка и дизайнерский контроль', description: ['После согласования дизайна, мы переходим к верстке', 'и выбираем, каким способом будет реализован сайт.', 'Я контролирую весь процесс, чтобы все было именно так,', 'как мы задумали.'], time: 'От 4-х дней' },
-    { id: '7', number: '07', title: 'Передача готового сайта и поддержка', description: ['Передаю вам все доступы и файлы сайта.'], time: '+1 месяц поддержки сайта' }
+    { id: '1', number: '01', title: 'Знакомство и бриф', description: ['Созвонимся, познакомимся и обсудим детали проекта,', 'чтобы я мог оценить стоимость и сроки.'],  description2: 'Созвонимся, познакомимся и обсудим детали проекта, чтобы я мог оценить стоимость и сроки.' , time: 'Займет 20-30 минут' },
+    { id: '2', number: '02', title: 'Анализ и структура', description: ['На этом этапе я изучаю целевую аудиторию, анализирую', 'рынок и конкурентов, составляю грамотную структуру,', 'чтобы предложить вам наиболее выигрышный вариант', 'реализации сайта'], description2: 'На этом этапе я изучаю целевую аудиторию, анализирую, рынок и конкурентов, составляю грамотную структуру, чтобы предложить вам наиболее выигрышный вариант, реализации сайта.', time: 'От 2-х дней' },
+    { id: '3', number: '03', title: 'Референсы и прототип', description: ['Я собираю все ваши пожелания по сайту, подбираю', 'качественные референсы, которые мы вместе и', 'утверждаем. После чего переходим к созданию прототипа.'], description2: 'Я собираю все ваши пожелания по сайту, подбираю, качественные референсы, которые мы вместе и, утверждаем. После чего переходим к созданию прототипа.', time: 'От 2-х дней' },
+    { id: '4', number: '04', title: 'Создание дизайн-концепции', description: ['Дизайню 1-3 ключевые страницы, в которых я', 'прорабатываю визуал будущего сайта. Этот этап помогает', 'найти общее виденье и создать грамотную концепцию.'], description2: 'Дизайню 1-3 ключевые страницы, в которых я, прорабатываю визуал будущего сайта. Этот этап помогает, найти общее виденье и создать грамотную концепцию.',  time: 'От 3-х дней' },
+    { id: '5', number: '05', title: 'Дизайн всего сайта и адаптивы', description: ['Отрисовываю весь сайт целиком, включая внутренние', 'страницы и их состояния в согласованном стиле. Рисую', 'адаптивные версии.'], description2: 'Отрисовываю весь сайт целиком, включая внутренние, страницы и их состояния в согласованном стиле. Рисую адаптивные версии.',  time: 'От 4-х дней' },
+    { id: '6', number: '06', title: 'Верстка и дизайнерский контроль', description: ['После согласования дизайна, мы переходим к верстке', 'и выбираем, каким способом будет реализован сайт.', 'Я контролирую весь процесс, чтобы все было именно так,', 'как мы задумали.'], description2: 'После согласования дизайна, мы переходим к верстке, и выбираем, каким способом будет реализован сайт. Я контролирую весь процесс, чтобы все было именно так, как мы задумали.', time: 'От 4-х дней' },
+    { id: '7', number: '07', title: 'Передача готового сайта и поддержка', description: ['Передаю вам все доступы и файлы сайта.'], description2: 'Передаю вам все доступы и файлы сайта.',  time: '+1 месяц поддержки сайта' }
   ];
 
   const [accordionStates, setAccordionStates] = useState(
@@ -71,7 +82,7 @@ const Services = () => {
     const initialDelay = 3800; // delay for first string
     const subsequentDelay = 100; // delay beetween strings
 
-    const lines = document.querySelectorAll('.servicesText p');
+    const lines = document.querySelectorAll('.servicesText p , .mobileText p');
     lines.forEach((_line, index) => {
       const delay = initialDelay + subsequentDelay * index;
       setTimeout(() => {
@@ -112,10 +123,32 @@ const Services = () => {
           </section>
         </section>
         <section className='servicesTextMobile'>
-          <h1><span className='firText'>Мои</span><span className='secText'> Услуги</span></h1>
+          <h1 data-splitting className='slide-vertical'><span className='firText'>Мои</span><span className='secText'> Услуги</span></h1>
           <section className='mobileText'>
-            <p>создаю продуманный дизайн,<br></br>сочетающий в себе эмоцию,<br></br>эстетику и удобство</p>
-            <p>К каждому проекту подхожу<br></br>как к собственному, всегда<br></br>выкладываюсь на  максимум</p>
+            {/* <p>создаю продуманный дизайн,<br></br>сочетающий в себе эмоцию,<br></br>эстетику и удобство</p>
+            <p>К каждому проекту подхожу<br></br>как к собственному, всегда<br></br>выкладываюсь на  максимум</p> */}
+            <section>
+            <p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
+              <span className='char'>создаю продуманный дизайн,</span>
+            </p>
+            <p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
+              <span className='char'>сочетающий в себе эмоцию,</span>
+            </p>
+            <p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
+              <span className='char'>эстетику и удобство</span>
+            </p>
+          </section>
+          <section>
+            <p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
+              <span className='char'>к каждому проекту подхожу</span>
+            </p>
+            <p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
+              <span className='char'>как к собственному, всегда</span>
+            </p>
+            <p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
+              <span className='char'>выкладываюсь на  максимум</span>
+            </p>
+          </section>
           </section>
         </section>
         <NavigationButtons />
@@ -166,10 +199,16 @@ const Services = () => {
                   <img src='accordionPlus.svg' alt='toggle' />
                 </label>
                 <section className='content'>
-                  {stage.description.map((line, index) => (
-                    <p key={index} className='description'>{line}</p>
-                  ))}
-                  <p className='time'>{stage.time}</p>
+                {isMobileView 
+                    ? <p className='description'>{stage.description2}</p>  // mobile string
+                    : (Array.isArray(stage.description) 
+                        ? stage.description.map((line, index) => (   // desktop array
+                            <p key={index} className='description'>{line}</p>
+                        ))
+                        : <p className='description'>{stage.description}</p>  // if description not array, else show string
+                    )
+                }
+                <p className='time'>{stage.time}</p>
                 </section>
               </li>
             ))}
