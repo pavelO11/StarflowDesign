48c62c2d060c7d8b62ad6934437b770cdaf6c39c
p1baby <lightuponce@gmail.com>
2025-01-06 20:23:14 +0500

    tryprojects

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,41 +1,93 @@
+// import './projects.scss'
+
 // import {
-//   MotionValue,
-//   useScroll,
-//   useTransform
+//     motion,
+//     MotionValue,
+//     useScroll,
+//     useTransform
 // } from "framer-motion"
 // import { useRef } from "react"
-// import './projects.scss'
+
+// const Projects: React.FC = () => {
 
 // function useParallax(value: MotionValue<number>, distance: number) {
 //   return useTransform(value, [0, 1], [-distance, distance]);
 // }
 
-import './projects.scss'
+// function Image({ id }: { id: number }) {
+//   const ref = useRef(null);
+//   const { scrollYProgress } = useScroll({ target: ref });
+//   const y = useParallax(scrollYProgress, 3);
 
-const Projects: React.FC = () => {
-  // const ref = useRef(null);
-  // const { scrollYProgress } = useScroll({ target: ref });
-  // const y = useParallax(scrollYProgress, 3);
+//   const images = [
+//     { id: '1', src: '/project1.svg' },
+//     { id: '2', src: '/project2.svg' },
+//     { id: '3', src: '/project3.svg' },
+//   ];
+
+//   return (
+//     <>
+//       <div ref={ref}>
+//         {images.map((image) => (
+//             <img alt='image' src={image.src} />
+//         ))}
+//       </div>
+//       <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2>
+//     </>
+//   );
+// }
+
+//     return (
+//         <section className='projectSection'>
+//             <Image id={0} />
+//         </section>
+//     );
+// }
+
+// export default Projects;
+import './projects.css'
+
+import {
+    motion,
+    MotionValue,
+    useScroll,
+    useTransform
+} from "framer-motion"
+import { useRef } from "react"
+
+// Хук для параллакса
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+// Компонент для одного изображения
+const ParallaxImage: React.FC<{ src: string; alt: string }> = ({ src, alt }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({ target: ref }); // Связь со скроллом секции
+  const y = useParallax(scrollYProgress, 300); // Параллакс для этого изображения
 
   return (
-    <section className='projectSection'>
-    {/* <div ref={ref}> */}
-      {/* <motion.section className="panel">
-        <motion.img className="bg" src='/projectsBg1.png' style={{ y }}/>
-      </motion.section>
-      <motion.section className="panel">
-        <motion.img className="bg" src='/projectsBg2.png' style={{ y }}/>
-      </motion.section>
-      <motion.section className="panel">
-        <motion.img className="bg" src='/projectsBg3.png' style={{ y }}/>
-      </motion.section> */}
-      {/* <img className='bg' src='/projectsBg1.png'/>
-      <img className='bg' src='/projectsBg2.png'/>
-      <img className='bg' src='/projectsBg3.png'/>
-    </div> */}
-    1
+    <section className="image-section">
+      <div ref={ref} className="image-container">
+        <motion.img src={src} alt={alt} className="image" style={{ y }} />
+      </div>
+      <motion.h2 className="parallax-text" style={{ y }}>
+        {alt}
+      </motion.h2>
     </section>
   );
-}
+};
+
+// Основной компонент
+const Projects: React.FC = () => {
+  return (
+    <main className="projects-container">
+      {/* Добавляем изображения */}
+      <ParallaxImage src="/project1.svg" alt="Project 1" />
+      <ParallaxImage src="/project2.svg" alt="Project 2" />
+      <ParallaxImage src="/project3.svg" alt="Project 3" />
+    </main>
+  );
+};
 
 export default Projects;
