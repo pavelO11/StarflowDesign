c2e62a511b867b8f57bdfaad84810774eb0bd017
p1baby <lightuponce@gmail.com>
2025-01-06 18:33:36 +0500

    add lenis scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { AnimatePresence } from 'framer-motion'
-// import Lenis from 'lenis'
 import { FC } from 'react'
 import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom'
 import Page404 from './pages/404/404'
@@ -11,71 +10,73 @@ import Services from './pages/services/Services'
 import './styles/default.scss'
 import './styles/variables.scss'
 
+// import Lenis from '@studio-freight/lenis/types'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+
 import { Footer, Navbar } from './components'
 
 const App: FC = () => {
-  return (
+    return (
     <Router>
         <AppContent />
     </Router>
-  );
-}
+    );
+    }
 
 const AppContent: FC = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-  // useEffect(() => {
-  //   // Lenis Animation onScroll
-  //   const lenis = new Lenis({
-  //     duration: 0.5,
-  //     easing: (t) => Math.min(1, 1.001 - Math.pow(3, -10 * t)), // smooth settings
-  //     // smoothWheel: true,
-  //     // smoothTouch: true,
-  //   });
-  
-  //   const handleScroll = (time: number) => {
-  //     lenis.raf(time); // Вызов анимации прокрутки Lenis
-  //     requestAnimationFrame(handleScroll);
-  //   };
-  
-  //   requestAnimationFrame(handleScroll);
-  
-  //   return () => {
-  //     lenis.destroy(); // clear Lenis
-  //   };
-  // }, []);
+    //Lenis Smooth Scroll
+    const lenis = new Lenis({
+        duration: 1.3,
+        easing: (t:number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+        orientation: "vertical", // vertical, horizontal
+        gestureOrientation: "vertical", // vertical, horizontal, both
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        syncTouch: false,
+        touchMultiplier: 1,
+        infinite: false
+    });
 
-  // Массив путей, на которых НЕ будет отображаться видео
-  const pagesWithoutVideo = ['/projects', '/services', '/about'];
+    function raf(time:number) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
 
-  // Проверка, не нужно ли показывать видео
-  const isVideoPage = !pagesWithoutVideo.includes(location.pathname);
+    // array route with no video
+    const pagesWithoutVideo = ['/projects', '/services', '/about'];
 
-  return (
+    // check needed video
+    const isVideoPage = !pagesWithoutVideo.includes(location.pathname);
+
+    return (
     <>
-      <section className="wrapper">
+        <section className="wrapper">
         {isVideoPage && (
-          <video autoPlay muted loop playsInline className="video-bg">
-            <source src="back.mp4" type="video/mp4" />
-          </video>
+            <video autoPlay muted loop playsInline className="video-bg">
+                <source src="back.mp4" type="video/mp4" />
+            </video>
         )}
         <section className="navbar">
-          <Navbar />
+            <Navbar />
         </section>
         <section className="content">
-          <AnimatePresence mode="wait">
+        <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
-              <Route path="/" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/404" element={<Page404 />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/404" element={<Page404 />} />
             </Routes>
-          </AnimatePresence>
+        </AnimatePresence>
         </section>
         <section className="footer">
-          <Footer />
+            <Footer />
         </section>
       </section>
     </>
