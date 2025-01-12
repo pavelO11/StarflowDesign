5c647aadb166465fb0884ce0ab335466147cc991
p1baby <p1babywork@gmail.com>
2025-01-12 13:52:15 +0300

    Add new compressed image + lenis + fix bug mix-blend-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { AnimatePresence } from 'framer-motion'
-import { FC } from 'react'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+import { FC, useEffect } from 'react'
 import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom'
+import { Footer, Navbar } from './components'
 import Page404 from './pages/404/404'
 import About from './pages/about/About'
 import Contacts from './pages/contacts/Contacts'
@@ -10,77 +13,86 @@ import Services from './pages/services/Services'
 import './styles/default.scss'
 import './styles/variables.scss'
 
-// import Lenis from '@studio-freight/lenis/types'
-import Lenis from 'lenis'
-import 'lenis/dist/lenis.css'
-
-import { Footer, Navbar } from './components'
-
 const App: FC = () => {
     return (
-    <Router>
-        <AppContent />
-    </Router>
+        <Router>
+            <AppContent />
+        </Router>
     );
-    }
+};
 
 const AppContent: FC = () => {
     const location = useLocation();
 
-    //Lenis Smooth Scroll
-    const lenis = new Lenis({
-        duration: 1.3,
-        easing: (t:number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-        orientation: "vertical", // vertical, horizontal
-        gestureOrientation: "vertical", // vertical, horizontal, both
-        smoothWheel: true,
-        wheelMultiplier: 1,
-        syncTouch: false,
-        touchMultiplier: 1,
-        infinite: false
-    });
+    // Array of routes where Lenis should not be active
+    const pagesWithoutLenis = ['/projects'];
 
-    function raf(time:number) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
+    // Initialize Lenis only if the current page is not in the pagesWithoutLenis array
+    useEffect(() => {
+        if (!pagesWithoutLenis.includes(location.pathname)) {
+            const lenis = new Lenis({
+                duration: 1.3,
+                easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+                orientation: "vertical", // vertical, horizontal
+                gestureOrientation: "vertical", // vertical, horizontal, both
+                smoothWheel: true,
+                wheelMultiplier: 1,
+                syncTouch: false,
+                touchMultiplier: 1,
+                infinite: false
+            });
 
-    // array route with no video
+            function raf(time: number) {
+                lenis.raf(time);
+                requestAnimationFrame(raf);
+            }
+
+            requestAnimationFrame(raf);
+
+            return () => {
+                lenis.destroy();
+            };
+        }
+    }, [location.pathname]);
+
+    // Array route with no video
     const pagesWithoutVideo = ['/projects', '/services', '/about'];
-
-    // check needed video
+    // Check needed video
     const isVideoPage = !pagesWithoutVideo.includes(location.pathname);
 
+    // Determine if the current page is the about page
+    const isAboutPage = location.pathname === '/about';
+
     return (
-    <>
-        <section className="wrapper">
-        {isVideoPage && (
-            <video autoPlay muted loop playsInline className="video-bg">
-                <source src="back.mp4" type="video/mp4" />
-            </video>
-        )}
-        <section className="navbar">
-            <Navbar />
-        </section>
-        <section className="content">
-        <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/404" element={<Page404 />} />
-            </Routes>
-        </AnimatePresence>
-        </section>
-        <section className="footer">
-            <Footer />
-        </section>
-      </section>
-    </>
-  );
+        <>
+            <section className="wrapper">
+                {isVideoPage && (
+                    <video autoPlay muted loop playsInline className="video-bg">
+                        <source src="back.mp4" type="video/mp4" />
+                    </video>
+                )}
+                <section className="navbar">
+                    <Navbar isAboutPage={isAboutPage} />
+                </section>
+                <section className="content">
+                    <AnimatePresence mode="wait">
+                        <Routes location={location} key={location.pathname}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/projects" element={<Projects />} />
+                            <Route path="/services" element={<Services />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/contacts" element={<Contacts />} />
+                            <Route path="/404" element={<Page404 />} />
+                            <Route path="*" element={<Page404 />} />
+                        </Routes>
+                    </AnimatePresence>
+                </section>
+                <section className="footer">
+                    <Footer />
+                </section>
+            </section>
+        </>
+    );
 };
 
 export default App;
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,72 +1,74 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import AboutMe from '../../components/aboutMe/AboutMe'
-import Principles from '../../components/principles/Principles'
-import './about.scss'
-
 import Splitting from 'splitting'
+import AboutMe from '../../components/aboutMe/AboutMe'
 import useSplittingHover from '../../components/hooks/useSplittingHover'
 import useSplittingOnLoad from '../../components/hooks/useSplittingOnLoad'
 import Curve from '../../components/layoutTransition'
 import NavigationButtons from '../../components/navigation/Navigation'
+import Principles from '../../components/principles/Principles'
+import './about.scss'
 
 const About = () => {
-	const [visibleLines, setVisibleLines] = useState<number[]>([]);
+    const [visibleLines, setVisibleLines] = useState<number[]>([]);
     const [principlesShow, setPrinciplesShow] = React.useState(false);
     const imageRef = useRef<HTMLDivElement>(null);
-    
+
     const handlePrinciplesShow = () => {
         setPrinciplesShow(!principlesShow);
     };
 
-		useEffect(() => { // add to body oveflow: hidden whe we see modal's
-			if (principlesShow) {
-				document.body.style.overflow = 'hidden';
-			} else {
-				document.body.style.overflow = '';
-			}
-		
-			return () => {
-				document.body.style.overflow = '';
-			};
-		}, [principlesShow]);
+    useEffect(() => {
+        if (principlesShow) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
 
-		useEffect(() => { // if we see aboutInro we add mix-blend-mode:normal to .navbar and if we dont see use difference
-			const observer = new IntersectionObserver(
-					([entry]) => {
-							const navbar = document.querySelector('.navbar') as HTMLElement;
-							if (navbar) {
-									if (entry.isIntersecting) {
-											navbar.style.mixBlendMode = 'normal';
-									} else {
-											navbar.style.mixBlendMode = 'difference';
-									}
-							}
-					},
-					{ threshold: 0.06 }
-			);
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, [principlesShow]);
 
-			if (imageRef.current) {
-					observer.observe(imageRef.current);
-			}
+    useEffect(() => {
+        const navbar = document.querySelector('.navbar') as HTMLElement;
+        if (navbar) {
+            navbar.style.mixBlendMode = 'normal'; // Initial mode for about page
+        }
 
-			return () => {
-					if (imageRef.current) {
-							observer.unobserve(imageRef.current);
-					}
-			};
-	}, []);
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                const navbar = document.querySelector('.navbar') as HTMLElement;
+                if (navbar) {
+                    if (entry.isIntersecting) {
+                        navbar.style.mixBlendMode = 'normal';
+                    } else {
+                        navbar.style.mixBlendMode = 'difference';
+                    }
+                }
+            },
+            { threshold: 0.06 }
+        );
 
-	useSplittingOnLoad('.slide-vertical');
+        if (imageRef.current) {
+            observer.observe(imageRef.current);
+        }
 
-	useSplittingHover();
+        return () => {
+            if (imageRef.current) {
+                observer.unobserve(imageRef.current);
+            }
+        };
+    }, []);
 
-	useEffect(() => {
+    useSplittingOnLoad('.slide-vertical');
+    useSplittingHover();
+
+    useEffect(() => {
         Splitting({ target: '.homeText p' });
-        
-        const initialDelay = 3800; // delay for first string
-        const subsequentDelay = 100; // delay beetween strings
 
+        const initialDelay = 3800; // delay for first string
+        const subsequentDelay = 100; // delay between strings
         const lines = document.querySelectorAll('.homeText p');
         lines.forEach((_line, index) => {
             const delay = initialDelay + subsequentDelay * index;
@@ -75,73 +77,72 @@ const About = () => {
             }, delay);
         });
     }, []);
-	
-    return(
-        <Curve>
-        {principlesShow && <Principles handlePrinciplesShow={handlePrinciplesShow}/> }
-		<section className='aboutSection'>
-			<section className='aboutIntro' ref={imageRef}>
-				<h1 data-splitting className='slide-vertical'><span className='firText'>Привет,</span><span className='secText'>Я Игорь</span></h1>
-				<article className='homeText'>
-					<section>
-						<p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
-							<span className='char'>Эмпатичный дизайнер с искренним</span>
-						</p>
-						<p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
-							<span className='char'>подходом, страстно увлеченный</span>
-						</p>
-						<p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
-							<span className='char'>своим любимым делом</span>
-						</p>
-					</section>
 
-					<section>
-						<p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
-							<span className='char'>Моя цель — объединить эстетику,</span>
-						</p>
-						<p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
-							<span className='char'>функциональность и значимость</span>
-						</p>
-						<p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
-							<span className='char'>в единое целое</span>
-						</p>
-					</section>
-                </article>
-				<NavigationButtons />
-				<section className='aboutFooter'>
-					<Link 
-					data-splitting 
-					to='https://dprofile.ru/starflowdesign/cv' 
-					target="_blank" 
-					rel="noopener noreferrer"
-					>
-						Резюме CV
-					</Link>
-					<a
-					data-splitting 
-					onClick={handlePrinciplesShow}
-					>
-						Принципы
-					</a>
-				</section>
-			</section>
-			<AboutMe/>
-			<section className='trump'>
-				<section className='textMain'>
-					<h2>Дизайн — это моя жизнь<br></br>Ваш успех — это моя цель</h2> 
-				</section>
-				<section className='textBottom'>
-					<p className='textBfirst'>Горю своим делом, вдохновляюсь<br></br>возможностью улучшения жизни других<br></br>людей с помощью дизайна и получаю<br></br>от этого безграничное удовольствие.</p>
-					<p className='textBsecond'>Сочетаю эстетичный дизайн с удобством,<br></br>постоянно совершенствую свои навыки,<br></br>чтобы ваш бизнес засиял также ярко,<br></br>как звёзды в ночном небе.</p>
-				</section>
-				<section className='textBottomMobile'>
-					<p className='textBfirst'>Горю своим делом, вдохновляюсь возможностью<br></br>улучшения жизни других людей с помощью дизайна<br></br> и получаю от этого безграничное удовольствие.</p>
-					<p className='textBsecond'>Сочетаю эстетичный дизайн с удобством постоянно<br></br>совершенствую свои навыки, чтобы ваш бизнес <br></br>засиял также ярко, как звёзды в ночном небе.</p>
-				</section>
-			</section>
-		</section>
-			</Curve>
-	)
-}
+    return (
+        <Curve>
+            {principlesShow && <Principles handlePrinciplesShow={handlePrinciplesShow} />}
+            <section className='aboutSection'>
+                <section className='aboutIntro' ref={imageRef}>
+                    <h1 data-splitting className='slide-vertical'>
+                        <span className='firText'>Привет,</span>
+                        <span className='secText'>Я Игорь</span>
+                    </h1>
+                    <article className='homeText'>
+                        <section>
+                            <p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
+                                <span className='char'>Эмпатичный дизайнер с искренним</span>
+                            </p>
+                            <p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
+                                <span className='char'>подходом, страстно увлеченный</span>
+                            </p>
+                            <p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
+                                <span className='char'>своим любимым делом</span>
+                            </p>
+                        </section>
+                        <section>
+                            <p className={visibleLines.includes(0) ? 'visible' : ''} data-splitting>
+                                <span className='char'>Моя цель — объединить эстетику,</span>
+                            </p>
+                            <p className={visibleLines.includes(1) ? 'visible' : ''} data-splitting>
+                                <span className='char'>функциональность и значимость</span>
+                            </p>
+                            <p className={visibleLines.includes(2) ? 'visible' : ''} data-splitting>
+                                <span className='char'>в единое целое</span>
+                            </p>
+                        </section>
+                    </article>
+                    <NavigationButtons />
+                    <section className='aboutFooter'>
+                        <Link
+                            data-splitting
+                            to='https://dprofile.ru/starflowdesign/cv'
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Резюме CV
+                        </Link>
+                        <a data-splitting onClick={handlePrinciplesShow}>
+                            Принципы
+                        </a>
+                    </section>
+                </section>
+                <AboutMe />
+                <section className='trump'>
+                    <section className='textMain'>
+                        <h2>Дизайн — это моя жизнь<br></br>Ваш успех — это моя цель</h2>
+                    </section>
+                    <section className='textBottom'>
+                        <p className='textBfirst'>Горю своим делом, вдохновляюсь<br></br>возможностью улучшения жизни других<br></br>людей с помощью дизайна и получаю<br></br>от этого безграничное удовольствие.</p>
+                        <p className='textBsecond'>Сочетаю эстетичный дизайн с удобством,<br></br>постоянно совершенствую свои навыки,<br></br>чтобы ваш бизнес засиял также ярко,<br></br>как звёзды в ночном небе.</p>
+                    </section>
+                    <section className='textBottomMobile'>
+                        <p className='textBfirst'>Горю своим делом, вдохновляюсь возможностью<br></br>улучшения жизни других людей с помощью дизайна<br></br> и получаю от этого безграничное удовольствие.</p>
+                        <p className='textBsecond'>Сочетаю эстетичный дизайн с удобством постоянно<br></br>совершенствую свои навыки, чтобы ваш бизнес <br></br>засиял также ярко, как звёзды в ночном небе.</p>
+                    </section>
+                </section>
+            </section>
+        </Curve>
+    );
+};
 
 export default About;
