807cda388af0e64b58830ecebaa6db95c65a94b5
p1baby <lightuponce@gmail.com>
2025-03-29 00:26:14 +0300

    minor fixes + update dev dependencies

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -223,6 +223,115 @@
 
 // export default Projects;
 
+// import { Helmet } from 'react-helmet'
+// import { Link } from 'react-router-dom'
+// import CustomCursor from '../../components/customCursor/CustomCursor'
+// import useSplittingOnLoad from '../../components/hooks/useSplittingOnLoad'
+// import Curve from '../../components/layoutTransition'
+// import Navigation from '../../components/navigation/Navigation'
+// import './projects.scss'
+
+// import { useEffect, useState } from 'react'
+// import arrowLeft from '/arrowLeft.svg'
+// import arrowRight from '/arrowRight.svg'
+
+// const projects = [
+//   {
+//     id: 1,
+//     src: "/REAL_ESTATE.jpg",
+//     first: "Real",
+//     second: " Estate",
+//     order: "00-1",
+//     link: "https://dprofile.ru/case/76591/real-estate"
+//   },
+//   {
+//     id: 2,
+//     src: "/SF_STORE.jpg",
+//     first: "SF —",
+//     second: " Store",
+//     order: "00-2",
+//     link: "https://dprofile.ru/case/55727/perfume-shop"
+//   },
+//   {
+//     id: 3,
+//     src: "/DETAILING_STUDIO.jpg",
+//     first: "Detailing",
+//     second: " Studio",
+//     order: "00-3",
+//     link: "https://dprofile.ru/case/47515/detailing-studio"
+//   }
+// ];
+
+// const Projects: React.FC = () => {
+//     const [isVisible, setIsVisible] = useState(false); // fade first loading
+
+//     useEffect(() => {
+//             setTimeout(() => {
+//                 setIsVisible(true);
+//             }, 4800); // delay
+//         }, []);
+
+//     useSplittingOnLoad('.slide-vertical');
+
+//   return (
+//     <Curve>
+//       <Helmet>
+//         <title>Starflow Design - Портфолио</title>
+//         <meta 
+//           name="description" 
+//           content="Ознакомьтесь с избранными работами Игоря Дубовцева, демонстрирующими опыт в UX & UI дизайне. Откройте для себя проекты, сочетающие эстетику, удобство и смыслы, направленные на усиление бизнеса." 
+//         />
+//       </Helmet>
+//       <main className="projects-container">
+//         {projects.map(project => (
+//           <Link 
+//             key={project.id} 
+//             to={project.link} 
+//             target="_blank" 
+//             rel="noopener noreferrer"
+//             className="image-link"
+//           >
+//             <section 
+//               className="image-container" 
+//               style={{ backgroundImage: `url(${project.src})` }}
+//             >
+//               <div className="mainText">
+//                 <h1 data-splitting className="slide-vertical">
+//                   <span className="firText">{project.first}</span>
+//                   <span className="secText">{project.second}</span>
+//                 </h1>
+//               </div>
+//               <div className="mainTextMobile">
+//                 <h1 data-splitting className="slide-vertical">
+//                   <span className="firText">{project.first}</span>
+//                   <span className="secText">{project.second}</span>
+//                 </h1>
+//                 <Link
+//                     className={`goProject ${isVisible ? 'fadeIn' : ''}`}
+//                     key={project.id} 
+//                     to={project.link} 
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                 <div>
+//                     <img className="leftArrow" src={arrowLeft} alt="arrow" />
+//                         СМОТРЕТЬ КЕЙС
+//                     <img className="rightArrow" src={arrowRight} alt="arrow" />
+//                 </div>
+//                 </Link>
+//               </div>
+//             </section>
+//           </Link>
+//         ))}
+//         <Navigation/>
+//         <CustomCursor />
+//       </main>
+//     </Curve>
+//   );
+// };
+
+// export default Projects;
+
 import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 import CustomCursor from '../../components/customCursor/CustomCursor'
@@ -231,94 +340,147 @@ import Curve from '../../components/layoutTransition'
 import Navigation from '../../components/navigation/Navigation'
 import './projects.scss'
 
+import { useEffect, useState } from 'react'
 import arrowLeft from '/arrowLeft.svg'
 import arrowRight from '/arrowRight.svg'
 
-const projects = [
-  {
-    id: 1,
-    src: "/REAL_ESTATE.jpg",
-    first: "Real",
-    second: " Estate",
-    order: "00-1",
-    link: "https://dprofile.ru/case/76591/real-estate"
-  },
-  {
-    id: 2,
-    src: "/SF_STORE.jpg",
-    first: "SF —",
-    second: " Store",
-    order: "00-2",
-    link: "https://dprofile.ru/case/55727/perfume-shop"
-  },
-  {
-    id: 3,
-    src: "/DETAILING_STUDIO.jpg",
-    first: "Detailing",
-    second: " Studio",
-    order: "00-3",
-    link: "https://dprofile.ru/case/47515/detailing-studio"
-  }
-];
-
 const Projects: React.FC = () => {
+    const [isVisible, setIsVisible] = useState(false); // fade first loading
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsVisible(true);
+        }, 4800); // delay
+    }, []);
+
     useSplittingOnLoad('.slide-vertical');
 
-  return (
-    <Curve>
-      <Helmet>
-        <title>Starflow Design - Портфолио</title>
-        <meta 
-          name="description" 
-          content="Ознакомьтесь с избранными работами Игоря Дубовцева, демонстрирующими опыт в UX & UI дизайне. Откройте для себя проекты, сочетающие эстетику, удобство и смыслы, направленные на усиление бизнеса." 
-        />
-      </Helmet>
-      <main className="projects-container">
-        {projects.map(project => (
-          <Link 
-            key={project.id} 
-            to={project.link} 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="image-link"
-          >
-            <section 
-              className="image-container" 
-              style={{ backgroundImage: `url(${project.src})` }}
-            >
-              <div className="mainText">
-                <h1 data-splitting className="slide-vertical">
-                  <span className="firText">{project.first}</span>
-                  <span className="secText">{project.second}</span>
-                </h1>
-              </div>
-              <div className="mainTextMobile">
-                <h1 data-splitting className="slide-vertical">
-                  <span className="firText">{project.first}</span>
-                  <span className="secText">{project.second}</span>
-                </h1>
+    return (
+        <Curve>
+            <Helmet>
+                <title>Starflow Design - Портфолио</title>
+                <meta
+                    name="description"
+                    content="Ознакомьтесь с избранными работами Игоря Дубовцева, демонстрирующими опыт в UX & UI дизайне. Откройте для себя проекты, сочетающие эстетику, удобство и смыслы, направленные на усиление бизнеса."
+                />
+            </Helmet>
+            <main className="projects-container">
                 <Link
-                    className="goProject"
-                    key={project.id} 
-                    to={project.link} 
+                    to="https://dprofile.ru/case/76591/real-estate"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="image-link"
                 >
-                <div>
-                    <img className="leftArrow" src={arrowLeft} alt="arrow" />
-                        СМОТРЕТЬ КЕЙС
-                    <img className="rightArrow" src={arrowRight} alt="arrow" />
-                </div>
+                    <section
+                        className="image-container"
+                        style={{ backgroundImage: `url(/REAL_ESTATE.jpg)` }}
+                    >
+                        <div className="mainText">
+                            <h1 data-splitting className="slide-vertical">
+                                <span className="firText">Real</span>
+                                <span className="secText"> Estate</span>
+                            </h1>
+                        </div>
+                        <div className="mainTextMobile">
+                            <h1 data-splitting className="slide-vertical">
+                                <span className="firText">Real</span>
+                                <span className="secText"> Estate</span>
+                            </h1>
+                            <Link
+                                className={`goProject ${isVisible ? 'fadeIn' : ''}`}
+                                to="https://dprofile.ru/case/76591/real-estate"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <div>
+                                    <img className="leftArrow" src={arrowLeft} alt="arrow" />
+                                    СМОТРЕТЬ КЕЙС
+                                    <img className="rightArrow" src={arrowRight} alt="arrow" />
+                                </div>
+                            </Link>
+                        </div>
+                    </section>
                 </Link>
-              </div>
-            </section>
-          </Link>
-        ))}
-        <Navigation/>
-        <CustomCursor />
-      </main>
-    </Curve>
-  );
+
+                <Link
+                    to="https://dprofile.ru/case/55727/perfume-shop"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="image-link"
+                >
+                    <section
+                        className="image-container"
+                        style={{ backgroundImage: `url(/SF_STORE.jpg)` }}
+                    >
+                        <div className="mainText">
+                            <h1 data-splitting className="slide-vertical">
+                                <span className="firText">SF —</span>
+                                <span className="secText"> Store</span>
+                            </h1>
+                        </div>
+                        <div className="mainTextMobile">
+                            <h1 data-splitting className="slide-vertical">
+                                <span className="firText">SF —</span>
+                                <span className="secText"> Store</span>
+                            </h1>
+                            <Link
+                                className={`goProject ${isVisible ? 'fadeIn' : ''}`}
+                                to="https://dprofile.ru/case/55727/perfume-shop"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <div>
+                                    <img className="leftArrow" src={arrowLeft} alt="arrow" />
+                                    СМОТРЕТЬ КЕЙС
+                                    <img className="rightArrow" src={arrowRight} alt="arrow" />
+                                </div>
+                            </Link>
+                        </div>
+                    </section>
+                </Link>
+
+                <Link
+                    to="https://dprofile.ru/case/47515/detailing-studio"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="image-link"
+                >
+                    <section
+                        className="image-container"
+                        style={{ backgroundImage: `url(/DETAILING_STUDIO.jpg)` }}
+                    >
+                        <div className="mainText">
+                            <h1 data-splitting className="slide-vertical">
+                                <span className="firText">Detailing</span>
+                                <span className="secText"> Studio</span>
+                            </h1>
+                        </div>
+                        <div className="mainTextMobile">
+                            <h1 data-splitting className="slide-vertical">
+                                <span className="firText">Detailing</span>
+                                <span className="secText"> Studio</span>
+                            </h1>
+                            <Link
+                                className={`goProject ${isVisible ? 'fadeIn' : ''}`}
+                                to="https://dprofile.ru/case/47515/detailing-studio"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <div>
+                                    <img className="leftArrow" src={arrowLeft} alt="arrow" />
+                                    СМОТРЕТЬ КЕЙС
+                                    <img className="rightArrow" src={arrowRight} alt="arrow" />
+                                </div>
+                            </Link>
+                        </div>
+                    </section>
+                </Link>
+
+                <Navigation />
+                <CustomCursor />
+            </main>
+        </Curve>
+    );
 };
 
 export default Projects;
