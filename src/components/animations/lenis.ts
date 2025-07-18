c1b309a43e554c78070ed32148db03ccfe08c29d
p1baby <lightuponce@gmail.com>
2025-07-18 23:08:16 +0300

    remove lenis from app, add new component lenis

---
+++ src/components/animations/lenis.ts
@@ -0,0 +1,25 @@
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+
+const lenis = new Lenis({
+    duration: 1.3,
+    easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    orientation: "vertical", // vertical, horizontal
+    gestureOrientation: "vertical", // vertical, horizontal, both
+    smoothWheel: true,
+    wheelMultiplier: 1,
+    syncTouch: false,
+    touchMultiplier: 1,
+    infinite: false
+});
+
+// Удаляем кастомные методы stop/start
+// Вместо этого будем использовать встроенные методы напрямую
+function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+export default lenis;
